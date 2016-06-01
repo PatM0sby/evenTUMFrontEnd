21236c3e42254880197ee2121cf46841257124dc
Andreas Horn <andreas.horn@tum.de>
2016-06-02 00:00:13 +0200

    Caterer dummy added

--- public/app/app.js
+++ public/app/app.js
@@ -2,10 +2,20 @@ var app = angular.module ("EvenTUM", [])
 
 app.controller ("HomeController", function($scope, $http){
     $scope.message = "Possible caterer";
+    $scope.caterer = {
+        "offerId" :  10030019,
+        "name" : "Name",
+        "descripton" : "This is an description lorem ipsum dolorem sit",
+        "pricePerPerson" : 19.50,
+        "kitchen" : "Chinese",
+        "selection" : "Snacks",
+        "vein" : ["chicken", "pig", "nut", "lactose"],
+        "user" : "90392390jif2j0c2mom2m0"
+    };
 
-    $http.get("http://localhost:3000/Caterer").success(function (response) {
+    /*$http.get("http://localhost:3000/Caterer").success(function (response) {
         $scope.Caterer = response;
     }).error(function(err){
        $scope.error = err;
-    });
-})
+    });*/
+});
