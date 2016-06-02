9caab5f1df1831a0016cf5a97518ab26a1fd78cd
Andreas Horn <andreas.horn@tum.de>
2016-06-02 02:30:16 +0200

    resolved wrong get uri

--- public/app/app.js
+++ public/app/app.js
@@ -2,9 +2,14 @@ var app = angular.module ("EvenTUM", ["ngRoute"])
 
 app.config(function($routeProvider){
     $routeProvider
-        .when("/Caterer", {
+        /*.when("/Caterer", {
             templateUrl: "app/templates/Caterer/list.html",
             controller: "CatererListController"
+        })*/
+        .when("/Location", {
+            templateUrl: "app/templates/Location/list.html",
+            controller: "LocationListController"
         })
-        .otherwise({redirectTo: '/Caterer'});
+        .otherwise({redirectTo: '/Location'});
+
 })
