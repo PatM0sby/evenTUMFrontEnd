58bf1889d05bb88a21cde736a3aa32b595165625
Flo <straussf@in.tum.de>
2016-06-23 23:27:50 +0200

    implemented create.event created new tables for create.event updated several things forgot the rest....

---
+++ public/app/controller/Event.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Flo on 23.06.16.
+ */
+/**
+ * Refaktoring, damit Dateien minifiziert werden können und trotzdem Funktionalität erhalten bleibt
+ */
+(function (angular) {
+    "use strict";
+
+    angular.module("EvenTUMEvent", ["ngRoute"])
+        .config(config);
+
+    // dependencies
+    config.$inject = ["$routeProvider"];
+
+    // functionality
+    function config ($routeProvider) {
+        $routeProvider
+            .when("/Event", {
+                templateUrl: "app/templates/Event/event.html"
+            });
+    }
+})(angular);
